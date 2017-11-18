fd951261004c24cd43231382d385584958729b9a
Herofied1180 <noahsturner06@gmail.com>
2017-11-17 23:59:54 -0500

    Inital Commit(Files)

---
+++ index.js
@@ -0,0 +1,57 @@
+	var Client = require('./Client');
+	module.exports.chat = {
+		send: function(message) {
+			gClient.sendArray([{m:"a", message: message}]);
+		}
+	}
+	var channel_id = "";
+	if(channel_id.substr(0, 1) == "/") channel_id = channel_id.substr(1);
+	if(channel_id == "") channel_id = "lobby";
+
+	var gClient = new Client("ws://www.multiplayerpiano.com");
+	module.exports.client = gClient;
+	gClient.setChannel(channel_id);
+	gClient.start();
+
+
+	// Setting status
+	(function() {
+		gClient.on("status", function(status) {
+			
+		});
+		gClient.on("count", function(count) {
+
+		});
+	})();
+
+	// Handle changes to participants
+	(function() {
+		gClient.on("participant added", function(part) {
+
+		});
+		gClient.on("participant removed", function(part) {
+
+		});
+		gClient.on("participant update", function(part) {
+
+		});
+		gClient.on("ch", function(msg) {
+			for(var id in gClient.ppl) {
+				if(gClient.ppl.hasOwnProperty(id)) {
+					var part = gClient.ppl[id];
+					
+				}
+			}
+		});
+		function updateCursor(msg) {
+			const part = gClient.ppl[msg.id];
+			
+		}
+		gClient.on("m", updateCursor);
+		gClient.on("participant added", updateCursor);
+		gClient.on("a", function(msg){
+			if (msg.a == "@info@") {
+				module.exports.chat.send('MPP.js made by: SonicCoding1180(FluffehKitteh48)')
+			}
+		});
+	})();
