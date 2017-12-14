78a22d56c4ccf723a3b02cad327f9fb316cb0f5d
Herofied1180 <noahsturner06@gmail.com>
2017-12-14 18:03:28 -0500

    Update

--- index.js
+++ index.js
@@ -12,6 +12,12 @@
 	module.exports.client = gClient;
 	gClient.setChannel(channel_id);
 	gClient.start();
+	module.exports.piano = {
+		keys: ["a0", "a1", "a2", "a3", "a4", "a5", "a6", "a-1", "as0", "as1", "as2", "as3", "as4", "as5", "as6", "as-1", "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b-1", "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "cs0", "cs1", "cs2", "cs3", "cs4", "cs5", "cs6", "d0", "d1", "d2", "d3", "d4", "d5", "d6", "ds0", "ds1", "ds2", "ds3", "ds4", "ds5", "ds6", "e0", "e1", "e2", "e3", "e4", "e5", "e6", "f0", "f1", "f2", "f3", "f4", "f5", "f6", "fs0", "fs1", "fs2", "fs3", "fs4", "fs5", "fs6", "fs7", "g0", "g1", "g2", "g3", "g4", "g5", "g6", "gs0", "gs1", "gs2", "gs3", "gs4", "gs5", "gs6"]
+	}
+	module.exports.press = function press(note, vel) {
+		gClient.startNote(note, vel);
+	}
 
 
 	// Setting status
@@ -22,7 +28,7 @@
 		gClient.on("count", function(count) {
 
 		});
-	})();
+	});
 
 	// Handle changes to participants
 	(function() {
